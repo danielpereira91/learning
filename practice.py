3e5c3aaab124f06f9234717c83c100ea89e3949d
Daniel Teixeira Pereira <97935464+danields91@users.noreply.github.com>
2023-06-30 14:47:52 -0300

    9º commit - Adicionei um arquivo python com um algoritmo simples para praticar

---
+++ practice.py
@@ -0,0 +1,8 @@
+name = input("Type your first name: ")
+age = int(input("Type your age: "))
+
+if age > 17:
+    print(f"Hello, {name}! You can start your studies with us. You are {age} years old.")
+else:
+    age_diff = 18 - age
+    print(f"Hello, {name}! We're sorry, but you'll have to wait to study with us. You are {age} years old. Come back when you're 18. It will take a maximum of {age_diff} year(s).")
